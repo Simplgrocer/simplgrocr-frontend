83ad497c623f8e5cc1e76e50859d8aff1815616e
“tirthyakamaldasgupta” <dasguptatirthyakamal@gmail.com>
2024-03-28 09:41:04 +0000

    feat: Basic navbar with one placeholder nav item.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { IndexComponent } from './components/index/index.component';
+import { IndexComponent } from './pages/index/index.component';
 
 export const routes: Routes = [
   {
