a31406e2b4347951b92918ee573d8149ab7aaceb
“tirthyakamaldasgupta” <dasguptatirthyakamal@gmail.com>
2024-03-29 13:50:22 +0000

    feat: All the styles are migrated from mdb to orange's boosted.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,6 @@ export const routes: Routes = [
   {
     path: 'grocery-list',
     component: GroceryListComponent,
-    title: 'Grocery list'
-  }
+    title: 'Grocery list',
+  },
 ];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MdbCollapseModule],
+  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
