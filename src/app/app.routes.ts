b2c25b76ef3cbaa81c16f113f1654202a63a8238
“tirthyakamaldasgupta” <dasguptatirthyakamal@gmail.com>
2024-04-20 05:25:19 +0000

    chore: Removed grocery list old component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
-import { IndexComponent } from './pages/index/index.component';
-import { GroceryListComponent } from './pages/grocery-list-old/grocery-list.component';
-import { LoginComponent } from './pages/login/login.component';
-import { OnboardingComponent } from './pages/onboarding/onboarding.component';
 import { authGuard } from './guards/auth.guard';
 import { GroceryListCreateComponent } from './pages/grocery-list-create/grocery-list-create.component';
 import { GroceryListViewUpdateComponent } from './pages/grocery-list-view-update/grocery-list-view-update.component';
+import { IndexComponent } from './pages/index/index.component';
+import { LoginComponent } from './pages/login/login.component';
+import { OnboardingComponent } from './pages/onboarding/onboarding.component';
 
 export const routes: Routes = [
   {
