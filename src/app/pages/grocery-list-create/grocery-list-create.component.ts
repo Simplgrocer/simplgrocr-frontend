0e161eaa0213da81a5e98aff3bb632b6968627f4
“tirthyakamaldasgupta” <dasguptatirthyakamal@gmail.com>
2024-04-27 15:11:48 +0000

    feat: Configured new list component.

--- src/app/pages/grocery-list-create/grocery-list-create.component.ts
+++ src/app/pages/grocery-list-create/grocery-list-create.component.ts
@@ -12,60 +12,94 @@ import {
   GroceryListService,
   UserGroceryListResponse,
 } from '../../services/grocery-list.service';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ToastModule } from 'primeng/toast';
+import { CenteredProgressSpinnerComponent } from '../../components/centered-progress-spinner/centered-progress-spinner.component';
+import { MessageService, PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-grocery-list-create',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, ContentEditableDirective],
+  providers: [MessageService],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ContentEditableDirective,
+    InputTextModule,
+    PasswordModule,
+    FloatLabelModule,
+    ButtonModule,
+    CardModule,
+    DividerModule,
+    ToastModule,
+    CommonModule,
+    CenteredProgressSpinnerComponent,
+  ],
   templateUrl: './grocery-list-create.component.html',
   styleUrl: './grocery-list-create.component.css',
 })
 export class GroceryListCreateComponent implements OnInit {
-  groceryListForm!: FormGroup;
-
-  groceryListFormStatus:
-    | 'NotSubmitted'
-    | 'Submitted'
-    | 'SubmissionError'
-    | 'InProgress' = 'NotSubmitted';
-
-  groceryListFormMessage!: string;
+  userGroceryListForm!: FormGroup;
+  disableUserGroceryListFormSubmission = true;
+  disableInteraction = false;
 
   constructor(
     private groceryListService: GroceryListService,
-    private router: Router
+    private router: Router,
+    private messageService: MessageService,
+    private primengConfig: PrimeNGConfig
   ) {}
 
   ngOnInit() {
-    this.groceryListForm = new FormGroup({
+    this.primengConfig.ripple = true;
+
+    this.userGroceryListForm = new FormGroup({
       name: new FormControl('', [Validators.required]),
       description: new FormControl(''),
       totalPrice: new FormControl(0),
     });
+
+    this.userGroceryListForm.statusChanges.subscribe((status) => {
+      if (status === 'VALID') {
+        this.disableUserGroceryListFormSubmission = false;
+      } else {
+        this.disableUserGroceryListFormSubmission = true;
+      }
+    });
   }
 
   createUserGroceryList() {
-    if (!this.groceryListForm.valid) {
+    if (!this.userGroceryListForm.valid) {
     }
 
-    this.groceryListFormStatus = 'InProgress';
+    this.disableInteraction = true;
 
     this.groceryListService
       .createUserGroceryList({
-        name: this.groceryListForm.controls['name'].value,
-        description: this.groceryListForm.controls['description'].value,
-        total_price: this.groceryListForm.controls['totalPrice'].value,
+        name: this.userGroceryListForm.controls['name'].value,
+        description: this.userGroceryListForm.controls['description'].value,
+        total_price: this.userGroceryListForm.controls['totalPrice'].value,
       })
       .subscribe({
         next: (response: UserGroceryListResponse) => {
-          this.groceryListFormStatus = 'InProgress';
+          this.disableInteraction = true;
 
           this.router.navigate([`/grocery-list/${response.id}`]);
         },
-        error: (err) => {
-          this.groceryListFormStatus = 'SubmissionError';
-          this.groceryListFormMessage =
-            'Apologies, there seems to be a technical issue. Our team is working on it. Please try again later. Thank you for your understanding.';
+        error: (error) => {
+          this.disableInteraction = false;
+
+          this.messageService.add({
+            key: 'tr',
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Unable to create grocery list',
+          });
         },
       });
   }
