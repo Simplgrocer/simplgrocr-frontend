6c34713aa3a116ce07f48bb41c33c9df1d4ba6ae
“tirthyakamaldasgupta” <dasguptatirthyakamal@gmail.com>
2024-05-01 04:09:58 +0000

    feat: Inclusion of viewing and updating basic form details and enabling and disabling of the basic form and the update button introduced.

--- src/app/pages/grocery-list-view-update/grocery-list-view-update.component.ts
+++ src/app/pages/grocery-list-view-update/grocery-list-view-update.component.ts
@@ -4,10 +4,12 @@ import {
   FormArray,
   FormControl,
   FormGroup,
+  FormsModule,
   ReactiveFormsModule,
-  Validators
+  Validators,
 } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { AccordionModule } from 'primeng/accordion';
 import {
   ConfirmationService,
@@ -24,17 +26,18 @@ import { InputNumberModule } from 'primeng/inputnumber';
 import { InputTextModule } from 'primeng/inputtext';
 import { MenubarModule } from 'primeng/menubar';
 import { PasswordModule } from 'primeng/password';
-import { SkeletonModule } from 'primeng/skeleton';
 import { ToastModule } from 'primeng/toast';
+import { ToggleButtonModule } from 'primeng/togglebutton';
 import { CenteredProgressSpinnerLgComponent } from '../../components/centered-progress-spinner-lg/centered-progress-spinner-lg.component';
 import { ProgressSpinnerLgComponent } from '../../components/progress-spinner-lg/progress-spinner-lg.component';
 import { ProgressSpinnerSmComponent } from '../../components/progress-spinner-sm/progress-spinner-sm.component';
 import { ContentEditableDirective } from '../../directives/content-editable.directive';
 import {
   GroceryListService,
+  GroceryListUpdationPayload,
   UserGroceryListResponse,
 } from '../../services/grocery-list.service';
-import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-grocery-list-view-update',
@@ -42,6 +45,7 @@ import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
   providers: [MessageService, ConfirmationService],
   imports: [
     CommonModule,
+    FormsModule,
     ReactiveFormsModule,
     ContentEditableDirective,
     MenubarModule,
@@ -57,7 +61,7 @@ import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
     DropdownModule,
     InputNumberModule,
     CommonModule,
-    SkeletonModule,
+    ToggleButtonModule,
     NgxSkeletonLoaderModule,
     ProgressSpinnerSmComponent,
     ProgressSpinnerLgComponent,
@@ -69,8 +73,16 @@ import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 export class GroceryListViewUpdateComponent implements OnInit {
   disableInteraction = false;
   userGroceryListBasicFormInitialized = false;
+  disableUserGroceryListBasicFormUpdationPreviousState = true;
+  disableUserGroceryListBasicFormUpdation = true;
+
+  edit = false;
+  editSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    this.edit
+  );
 
   id: string | undefined;
+  userGroceryList: UserGroceryListResponse | undefined;
   userGroceryListForm: FormGroup | undefined;
 
   constructor(
@@ -90,11 +102,37 @@ export class GroceryListViewUpdateComponent implements OnInit {
     if (this.id) {
       this.groceryListService.getUserGroceryList(this.id).subscribe({
         next: (response: UserGroceryListResponse) => {
+          this.userGroceryList = response;
+
           this.userGroceryListForm = new FormGroup({
             name: new FormControl(response.name, [Validators.required]),
             description: new FormControl(response.description),
             totalPrice: new FormControl(response.total_price),
             items: new FormArray([]),
+          });
+
+          this.userGroceryListForm.valueChanges.subscribe(() => {
+            const nameControl = this.userGroceryListForm?.get('name')!;
+            const descriptionControl =
+              this.userGroceryListForm?.get('description')!;
+
+            const nameChanged =
+              nameControl.value !== this.userGroceryList?.name;
+            const descriptionChanged =
+              descriptionControl.value !== this.userGroceryList?.description;
+
+            const nameIsValid = nameControl.valid;
+            const descriptionIsValid = descriptionControl.valid;
+
+            this.disableUserGroceryListBasicFormUpdation = !(
+              nameIsValid &&
+              descriptionIsValid &&
+              this.edit &&
+              (nameChanged || descriptionChanged)
+            );
+
+            this.disableUserGroceryListBasicFormUpdationPreviousState =
+              this.disableUserGroceryListBasicFormUpdation;
           });
 
           this.userGroceryListBasicFormInitialized = true;
@@ -106,5 +144,54 @@ export class GroceryListViewUpdateComponent implements OnInit {
         },
       });
     }
+  }
+
+  onEditChange(value: boolean): void {
+    if (!value) {
+      this.disableUserGroceryListBasicFormUpdation = true;
+    } else {
+      if (!this.disableUserGroceryListBasicFormUpdationPreviousState) {
+        this.disableUserGroceryListBasicFormUpdation = false;
+      }
+    }
+  }
+
+  updateUserGroceryListBasicDetails(): void {
+    this.disableInteraction = true;
+
+    const name = this.userGroceryListForm!.controls['name'].value;
+    const description = this.userGroceryListForm!.controls['description'].value;
+
+    let userGroceryListUpdationPayload: GroceryListUpdationPayload = {};
+
+    if (name !== this.userGroceryList!.name) {
+      userGroceryListUpdationPayload.name = name;
+    }
+
+    if (description !== this.userGroceryList!.description) {
+      userGroceryListUpdationPayload.description = description;
+    }
+
+    this.groceryListService
+      .updatePatchUserGroceryList(this.id!, userGroceryListUpdationPayload)
+      .subscribe({
+        next: () => {
+          this.disableInteraction = false;
+
+          this.disableUserGroceryListBasicFormUpdation = true;
+        },
+        error: (error) => {
+          this.disableInteraction = false;
+
+          this.messageService.add({
+            key: 'tr',
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Unable to update grocery list',
+          });
+
+          this.disableUserGroceryListBasicFormUpdation = true;
+        },
+      });
   }
 }
