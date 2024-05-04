2fff85eb3d951e1e5bf38f2a6d92a813ef5379d8
“tirthyakamaldasgupta” <dasguptatirthyakamal@gmail.com>
2024-05-04 12:35:06 +0000

    feat: Included a banner in homepage when there are no items.

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -7,17 +7,31 @@ import { Router } from '@angular/router';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { CenteredProgressSpinnerLgComponent } from '../../components/centered-progress-spinner-lg/centered-progress-spinner-lg.component';
+import { MessagesModule } from 'primeng/messages';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [CardModule, ButtonModule, CenteredProgressSpinnerLgComponent],
+  imports: [
+    CardModule,
+    ButtonModule,
+    MessagesModule,
+    CenteredProgressSpinnerLgComponent,
+  ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css',
 })
 export class IndexComponent implements OnInit {
   disableInteraction = true;
   groceryLists: UserGroceryListResponse[] = [];
+
+  itemsNotFoundBanner = [
+    {
+      severity: 'info',
+      detail:
+        'This section is pretty empty. Please add some items to your grocery list.',
+    },
+  ];
 
   constructor(
     private router: Router,
